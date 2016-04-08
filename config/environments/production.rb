28da9058cb17a625a268846c84becf86906de7a4
Jamie <jamiesingh96@gmail.com>
2016-04-08 04:02:05 +0000

    error in compiling fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,8 +7,8 @@ Rails.application.configure do
   config.eager_load = true
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-  config.serve_static_files = false
-  config.assets.js_compressor = :uglifier 
+  config.serve_static_files = true
+  config.assets.js_compressor = Uglifier.new(mangle: false)  
   config.assets.compile = true
   config.assets.digest = true
   config.log_level = :debug
